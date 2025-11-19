ef4333a74d95fac62370f3a7509970e41ca672f6
tawhidislam22 <tawhidislam144@gmail.com>
2025-11-19 17:14:43 +0600

    fix it

--- lib/scraper.ts
+++ lib/scraper.ts
@@ -3,7 +3,7 @@ import * as cheerio from 'cheerio';
 
 export async function findLinkedInFromDuckDuckGo(urls: string[]) {
   const browser = await playwright.chromium.launch({ 
-    headless: true,
+    headless: false,
     args: [
       '--no-sandbox', 
       '--disable-setuid-sandbox', 
